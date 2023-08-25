31b45b4691abed52d6e9fb4720d5dfb4fe249769
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-25 14:22:15 +0200

    Install ErrorOr package, create Errors.Breakfast service, update GetBreakfast action to use custom error handler

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -1,6 +1,8 @@
 using BuberBreakfast.Contracts.BuberBreakfast;
 using BuberBreakfast.Models;
+using BuberBreakfast.ServiceErrors;
 using BuberBreakfast.Services.Breakfasts;
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
@@ -50,7 +52,14 @@ public class BreakfastsController : ControllerBase
     [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
-        Breakfast breakfast = _breakfastService.GetBreakfast(id);
+        ErrorOr<Breakfast> getBreakfastResult = _breakfastService.GetBreakfast(id);
+
+        if (getBreakfastResult.IsError && getBreakfastResult.FirstError == Errors.Breakfast.NotFound)
+        {
+            return NotFound();
+        }
+
+        var breakfast = getBreakfastResult.Value;
 
         var response = new BreakfastResponse(
             breakfast.Id,
