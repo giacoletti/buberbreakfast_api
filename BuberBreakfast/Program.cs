be96f87a39c0b2e1564182d2c711a0440177d4cb
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-25 13:28:39 +0200

    Add exception handler in Program.cs, create ErrorsController

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -20,10 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
